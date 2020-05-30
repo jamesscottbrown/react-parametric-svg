c98610a0dd11647962af4100486bdf43f47eaad4
James Scott-Brown <james@jamesscottbrown.com>
2020-05-30 20:35:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# React parametric SVG
+
+This is a [React](https://reactjs.org/) component to render a parametric SVG.
+
+It should be considered at an early stage of development, and may contain bugs.
+
+Install using `npm install react-parametric-svg`.
+
+## Usage
+
+
+Render using default parameter values:
+
+```jsx
+import React from 'react';
+import {ParamatericSVG} from 'react-parametric-svg';
+
+const ExampleComponent = () => {
+    const defaultSVGString = `<svg  version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:parametric="https://parametric-svg.github.io/v0.2"
+        width="500"
+        height="500"
+        parametric:defaults="x=60;y=60;d=5">
+        <circle parametric:cx="{x + d}" parametric:cy="{y}" r="40" stroke="black" stroke-width="3" fill="blue" />
+        <circle parametric:cx="{x}" parametric:cy="{y}" r="40" stroke="black" stroke-width="3" fill="green" />
+        </svg>`;
+
+    return <ParamatericSVG svgString={defaultSVGString} />;
+}
+
+```
+
+You can provide an object of values for the parameters (you don't need to specify values for all of them, as the values you provide will be merged with the defaults specified in the SVG):
+
+```jsx
+<ParamatericSVG svgString={defaultSVGString} params={{d: 100}} />
+```
+
+You can set `innerOnly={true}` to render just the contents of the SVG, not the actual SVG element itself.
+This allows you to combine several parametric SVGs into a single SVG:
+
+```jsx
+
+<svg width="400" height="400">
+    <g transform="translate(0, 0)">
+        <ParamatericSVG svgString={defaultSVGString} params={{d: 25}} innerOnly={true}/>
+    </g>
+    <g transform="translate(0, 100)">
+        <ParamatericSVG svgString={defaultSVGString} params={{d: 50}} innerOnly={true}/>
+    </g>
+    <g transform="translate(0, 200)">
+        <ParamatericSVG svgString={defaultSVGString} params={{d: 100}} innerOnly={true}/>
+    </g>
+</svg>
+```
---
+++ src/index.js
@@ -0,0 +1,178 @@
+import React, {useEffect} from 'react';
+import * as math from 'mathjs';
+
+
+const processExpressionTerm = (term, parameters) => {
+    const expression = term
+        .replace(/\{/g, '')
+        .replace(/\(/g, '')
+        .replace(/\)/g, '')
+        .replace(/\}/g, '')
+        .replace(/\+/g, ' ')
+        .replace(/\-/g, ' ')
+        .replace(/\^/g, ' ')
+        .replace(/\//g, ' ')
+        .replace(/\*/g, ' ')
+        .replace(/,/g, ' ')
+        .replace(/'/g, ' ');
+
+    const terms = expression.split(" ");
+
+    for (let term in terms) {
+        if (!terms[term] || !isNaN(terms[term])) {
+            continue;
+        }
+
+        if (!parameters[terms[term]]) {
+            parameters[terms[term]] = 0;
+        }
+    }
+
+    return parameters;
+}
+
+const getParameters = (attributes, parameters) => {
+    // Given a NamedNodeMap or Array of attributes for a tag, identify any parameters not recorded in the parameters array
+
+    if (!attributes) {
+        return;
+    }
+
+    // process only the contents of {}
+    const re = /\{(.+?)\}/g;
+
+
+    for (let j = 0; j < attributes.length; j++) {
+        const attribute = attributes[j];
+
+        if (attribute.name.startsWith("parametric:")) {
+            attribute.nodeValue.replace(re, (match, g1, g2) => {
+                parameters = processExpressionTerm(g1, parameters);
+            });
+        }
+    }
+};
+
+
+const getDefaultParamValues = (svg, defaultParameters) => {
+    const defaultAttribute = svg.attributes["parametric:defaults"];
+    if (defaultAttribute) {
+        let assignments = defaultAttribute.nodeValue.split(";");
+        let param, value;
+        for (let i in assignments) {
+            [param, value] = assignments[i].split("=");
+            param = param.trim();
+            defaultParameters[param] = value;
+        }
+    }
+
+    return defaultParameters;
+}
+
+const getTree = (parentNode) => {
+    const childrenArray = [].slice.call(parentNode.children)
+    const children = childrenArray.map(child => getTree(child));
+
+    return {
+        tagName: parentNode.tagName,
+        id: parentNode.id,
+        className: parentNode.className,
+        attributes: [...parentNode.attributes].map(d =>
+            ({name: d.name, value: d.value})),
+        children: children
+    };
+}
+
+// This is the intended entrypoint for the parametric SVG editor
+const parseParametricSVG = (svgString) => {
+    var virtualParent = document.createElement('div'); // can't set outerHTML of an element with no parent
+    var virtualSVG = document.createElement('svg');
+    virtualSVG.id = 'VIRTUAL-SVG';
+
+    virtualParent.appendChild(virtualSVG);
+
+    if (svgString.includes("<svg")) {
+        virtualSVG.outerHTML = svgString;
+    } else {
+        virtualSVG.innerHTML = svgString;
+    }
+    virtualSVG = virtualParent.lastChild;
+
+    let parameters = {};
+    let defaultParameters = {};
+
+    let children = virtualSVG.childNodes;
+    for (let i = 0; i < children.length; i++) {
+        getParameters(children[i].attributes, parameters);
+    }
+
+    defaultParameters = getDefaultParamValues(virtualSVG, defaultParameters);
+
+    // Construct tree in JSON reflecting tree in SVG
+    const tree = getTree(virtualSVG);
+
+    return {parameters, defaultParameters, tree};
+}
+
+
+///
+
+
+const re = /\{(.+?)\}/g;
+
+const Element = ({tree, params, defaultParams}) => {
+    var attributes = {};
+    const attribute_names = tree.attributes.map(d => d.name);
+
+    for (let i in tree.attributes) {
+        let name = tree.attributes[i].name;
+        let value = tree.attributes[i].value;
+
+
+        if (name && name.startsWith("parametric:")) {
+
+            attributes[name] = value; // save the parametric version
+
+            value = value.replace(re, function (match, g1, g2) {
+                try {
+                    return math.evaluate(g1, params)
+                } catch (err) {
+                    return null;
+                }
+
+            });
+            attributes[name.replace('parametric:', '')] = value; // save version with paramater values substituted
+        } else if (!attribute_names.includes('parametric:' + name)) {
+            attributes[name] = value;
+        } else {
+            // this is the non-parametric version of an attribute: don't replace value substituted above
+        }
+    }
+
+    const childTags = tree.children.map( (child, i) => <Element tree={child} params={params} defaultParams={defaultParams} key={i} />);
+    return React.createElement(tree.tagName, attributes, childTags);
+};
+
+
+const PreviewSVG = ({params, defaultParams, tree, updateSVGString}) => {
+    useEffect(() => {if (updateSVGString){ updateSVGString()}});
+    return <Element tree={tree} params={params} defaultParams={defaultParams}/>;
+};
+
+
+// This is the entrypoint if you just want to render a parametric SVG
+const ParamatericSVG = ({svgString, params, onParse, innerOnly}) => {
+    const {parameters, defaultParameters: defaultParams, tree} = parseParametricSVG(svgString);
+
+    if (onParse){
+        onParse(parameters, defaultParams);
+    }
+
+    if (innerOnly){
+        return <>{tree.children.map( (child, i) => <Element tree={child} params={{...defaultParams, ...(params || {})}} defaultParams={defaultParams} key={i} />)}</>
+    } else {
+        return <Element tree={tree} params={{...defaultParams, ...(params || {})}} defaultParams={defaultParams}/>;
+    }
+}
+
+export {ParamatericSVG, parseParametricSVG, PreviewSVG};
