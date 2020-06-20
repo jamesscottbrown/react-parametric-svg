accb1d39001e3682e592cd384cee6a048e592956
James Scott-Brown <james@jamesscottbrown.com>
2020-06-20 15:45:33 +0100

    fix typo in comment

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 // This file is needed to get Jest to call Babel
-// without it Est will not understand the JSX
+// without it Jest will not understand the JSX
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
 };
